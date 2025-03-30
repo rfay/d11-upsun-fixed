2786f1393aeeea02d5070909032dc9d340eacb52
Randy Fay <randy@randyfay.com>
2025-03-30 16:35:40 -0600

    Update composer

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -882,3 +882,8 @@ if (getenv('PLATFORM_PROJECT_ENTROPY') != "") {
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+// Include settings required for Redis cache.
+if ((file_exists(__DIR__ . '/settings.ddev.redis.php') && getenv('IS_DDEV_PROJECT') == 'true')) {
+  include __DIR__ . '/settings.ddev.redis.php';
+}
