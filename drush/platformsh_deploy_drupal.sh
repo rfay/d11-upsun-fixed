57e100ac41f3af7521979f607a7598eb801e2621
Randy Fay <randy@randyfay.com>
2025-09-25 12:29:59 -0600

    Use platform_deploy/generate not upsun

--- drush/platformsh_deploy_drupal.sh
+++ drush/platformsh_deploy_drupal.sh
@@ -2,6 +2,9 @@
 #
 # We don't want to run drush commands if drupal isn't installed.
 # Similarly, we don't want to attempt to run config-import if there aren't any config files to import
+# @todo expand further to pass --uri for all sites, with an eye towards multisite
+#
+
 
 if [ -n "$(drush status --field=bootstrap)" ]; then
   drush -y cache-rebuild
